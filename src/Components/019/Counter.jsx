80596219013f2863771125944c5f2d0017c47de3
LeoGytis <leogytis@gmail.com>
2022-09-26 13:06:44 +0300

    019 lesson done

---
+++ src/Components/019/Counter.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {counter: 0};
+    }
+
+    doCklick = () => {
+        this.setState(s => ({counter: s.counter + 1}));
+    }
+
+    componentDidMount() {
+        console.log('LOaded');
+    }
+
+    componentWillUnmount() {
+        console.log('numiro');
+    }
+
+    componentDidUpdate() {
+        console.log('apsiapdeitino');
+    }
+
+    render() {
+        return (
+            <>
+            <div>{this.state.counter}: {this.props.when}</div>
+            <button onClick={this.doCklick}>
+            <svg>
+                <use href="#star"/>
+            </svg>
+            <svg className="kitas-svg">
+                <use href="#star"/>
+            </svg>
+            <svg>
+                <use href="#star"/>
+            </svg>
+            </button>
+            </>
+        )
+    }
+}
+
+export default Counter;
