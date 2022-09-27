f434eb137890b908050d4b1f73114184ad9fa486
LeoGytis <leogytis@gmail.com>
2022-09-27 17:29:05 +0300

    viskas vyksta

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import "./App.scss";
 import "./crud.scss";
 import { useReducer } from "react";
 import listReducer from "./Reducers/listReducer";
-
+import { useState } from "react";
 
 function App() {
-    // const [numb, dispachNumber] = useReducer(numberReducer, "0000");
-
     const [list, listDispachd] = useReducer(listReducer, []);
-    
+    // const [sortedList, getSortedList] = useState[list];
 
     const getNewList = () => {
         const action = {
             type: "new_list",
         };
         listDispachd(action);
+    };
 
+    const sortList = () => {
+        const action = {
+            type: "sort",
+        };
+        listDispachd(action);
     }
 
     return (
@@ -23,16 +27,24 @@ function App() {
             <header className="App-header">
                 <h1>021 lesson - Reducer uzduotys</h1>
                 <button
-                        className="btn btn-outline-success mt-2"
-                        onClick={getNewList}
-                    >
-                        Get New List
-                    </button>
-                    <div className="kvc">
-                    {list.map((l, i) => (
-                        <div className="kv" key={i}>{l}</div>
+                    className="btn btn-outline-success mt-2"
+                    onClick={getNewList}
+                >
+                    Get New List
+                </button>
+                <div className="kvc">
+                    {list.map((a, i) => (
+                        <div className="kv" style={{backgroundColor :a.color}} key={i}>
+                            {a.number}
+                        </div>
                     ))}
                 </div>
+                <button
+                    className="btn btn-outline-success mt-2"
+                    onClick={sortList}
+                >
+                    Sort the List
+                </button>
 
             </header>
         </div>
--- src/Reducers/listReducer.js
+++ src/Reducers/listReducer.js
@@ -1,4 +1,5 @@
-import rand from "../Functions/randomNumber";
+import randomColor from "../Functions/randomColor";
+import randomNumber from "../Functions/randomNumber";
 
 function listReducer(state, action) {
     let newState;
@@ -8,12 +9,21 @@ function listReducer(state, action) {
         case "new_list":
             newState = [];
             for (let i = 0; i < 10; i++) {
-                newState.push(("" + rand(0, 9999)).padStart(4, 0));
+                // const obj = {number: randomNumber(0, 1000), color: 'green'};
+                // newState.push(("" + rand(0, 9999)).padStart(4, 0));
+                // newState.push(randomNumber(0, 1000));
+                // newState.push(obj);
+                newState.push({number: randomNumber(0, 1000), color: randomColor()});
             }
+            break;
+        case "sort":
+            newState = [...state].sort((a, b) =>  {
+                return a.number - b.number;
+              });
             break;
 
         default:
-            newState = state;
+            newState = [...state];
     }
 
     return newState;
