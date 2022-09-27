2edc3bd3fb94e5faa585428dea07146e60009bc5
LeoGytis <leogytis@gmail.com>
2022-09-27 17:55:33 +0300

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,21 @@ function App() {
             type: "sort",
         };
         listDispachd(action);
-    }
+    };
+
+    const more500 = () => {
+        const action = {
+            type: "more500",
+        };
+        listDispachd(action);
+    };
+
+    const less400 = () => {
+        const action = {
+            type: "less400",
+        };
+        listDispachd(action);
+    };
 
     return (
         <div className="App">
@@ -32,20 +46,35 @@ function App() {
                 >
                     Get New List
                 </button>
-                <div className="kvc">
-                    {list.map((a, i) => (
-                        <div className="kv" style={{backgroundColor :a.color}} key={i}>
-                            {a.number}
-                        </div>
-                    ))}
-                </div>
                 <button
                     className="btn btn-outline-success mt-2"
                     onClick={sortList}
                 >
                     Sort the List
                 </button>
-
+                <div className="kvc">
+                    {list.map((a, i) => a.show ? 
+                        <div
+                            className="kv"
+                            style={{ backgroundColor: a.color }}
+                            key={i}
+                        >
+                            {a.number}
+                        </div> : null
+                    )}
+                </div>
+                <button
+                    className="btn btn-outline-success mt-2"
+                    onClick={more500}
+                >
+                    More than 500
+                </button>
+                <button
+                    className="btn btn-outline-success mt-2"
+                    onClick={less400}
+                >
+                    Less than 400
+                </button>
             </header>
         </div>
     );
--- src/Reducers/listReducer.js
+++ src/Reducers/listReducer.js
@@ -13,13 +13,32 @@ function listReducer(state, action) {
                 // newState.push(("" + rand(0, 9999)).padStart(4, 0));
                 // newState.push(randomNumber(0, 1000));
                 // newState.push(obj);
-                newState.push({number: randomNumber(0, 1000), color: randomColor()});
+                newState.push({
+                    number: randomNumber(0, 1000),
+                    color: randomColor(),
+                    show: true,
+                });
             }
             break;
         case "sort":
-            newState = [...state].sort((a, b) =>  {
+            newState = [...state].sort((a, b) => {
                 return a.number - b.number;
-              });
+            });
+            break;
+
+        case "more500":
+            // newState = [...state].filter((a) => {return a.number > 500;});
+            newState = state.map((o) =>
+                o.number > 500 ? { ...o, show: true } : { ...o, show: false }
+            );
+
+            break;
+
+        case "less400":
+            // newState = [...state].filter((a) => {return a.number < 400;});
+            newState = state.map((o) =>
+                o.number < 400 ? { ...o, show: true } : { ...o, show: false }
+            );
             break;
 
         default:
