50616a3587ff3c79eed93bee989bc4a8b9e25449
LeoGytis <leogytis@gmail.com>
2022-09-28 15:26:12 +0300

    books reducer

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,40 @@ function App() {
     const [books, dispachBooks] = useReducer(booksReducer, []);
 
 
-    const getNewList = (responseData) => {
+    // const getNewList = (responseData) => {
+    //     const action = {
+    //         type: "new_list",
+    //         payload: responseData
+    //     };
+    //     dispachBooks(action);
+    // };
+
+    useEffect(() => {
+        axios.get("https://in3.dev/knygos/").then((res) => {
+            const action = {
+                type: "new_list",
+                payload: res.data
+            };
+            console.log(res.data);
+
+            dispachBooks(action);
+            // getNewList(res.data);
+        });
+    }, []);
+
+       const sortList = () => {
         const action = {
-            type: "new_list",
-            payload: responseData
+            type: "sort_list",
         };
         dispachBooks(action);
     };
 
-    useEffect(() => {
-        axios.get("https://in3.dev/knygos/").then((res) => {
-            // const action = {
-            //     type: "new_list",
-            //     payload: res.data
-            // };
-            // dispachBooks(action);
-            getNewList(res.data);
-        });
-    }, []);
-
+    const defaultSortList = () => {
+        const action = {
+            type: "defaultSortList",
+        };
+        dispachBooks(action);
+    };
  
 
 
@@ -35,6 +50,18 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>022 Lesson</h1>
+                <button
+                        className="btn btn-outline-success mt-2 ms-3"
+                        onClick={sortList}
+                    >
+                        Sort the List
+                    </button>
+                    <button
+                        className="btn btn-outline-success mt-2 ms-3"
+                        onClick={defaultSortList}
+                    >
+                        Default sort list
+                    </button>
                 <div>
                     {
                        books.length ? books.map(b => <div key={b.id}>{b.title} <i>{b.price} EUR</i></div>) : <h2>Loading...</h2>
--- src/Reducers/booksReducer.js
+++ src/Reducers/booksReducer.js
@@ -1,14 +1,32 @@
 function booksReducer(state, action) {
     let newState;
-    newState = state;
+    // newState = state;
 
     switch (action.type) {
         case "new_list":
             newState = action.payload.map((b, i) => ({ ...b, row: i }));
             break;
-            default:
-                newState = [...state];
-        }
+
+        case "sort_list":
+            // newState = [...state].sort((a, b) => {   // NOT WORKING
+            //     return a.title - b.title;
+            // });
+            newState = [...state].sort((a, b) => {
+                if (a.title > b.title) return 1;
+                if (b.title > a.title) return -1;
+                return 0;
+              });
+            break;
+
+        case "defaultSortList":
+            newState = [...state].sort((a, b) => {
+                return a.row - b.row;
+            });
+            break;
+
+        default:
+            newState = [...state];
+    }
 
     return newState;
 }
