bc7bbb96f4c063f3215621840135e9be0e4acfa1
LeoGytis <leogytis@gmail.com>
2022-09-28 14:21:59 +0300

    022 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.scss";
-import "./crud.scss";
+import { useEffect, useState } from "react";
+import Books from "./Components/011/Books";
+import axios from "axios";
 
 function App() {
-   
+    const [books, setBooks] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://in3.dev/knygos/").then((res) => {
+            setBooks(res.data);
+        });
+    }, []);
+
+
     return (
         <div className="App">
             <header className="App-header">
-               
-            
+                <h1>022 Lesson</h1>
+                <div>
+                    {
+                       books.length ? books.map(b => <div key={b.id}>{b.title} <i>{b.price} EUR</i></div>) : <h2>Loading...</h2>
+                    }
+                </div>
             </header>
         </div>
     );
